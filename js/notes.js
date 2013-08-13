070b2050c53768a65788a39380e7538201f2b1bc
Chris Thomson <chris@cthomson.ca>
2013-08-13 00:10:16 -0400

    Notes: have a set ordering on courses, if specified

--- js/notes.js
+++ js/notes.js
@@ -5,15 +5,21 @@ $(document).ready(function() {
     success: function(documents) {
       for (doc_name in documents) {
         if (documents.hasOwnProperty(doc_name)) {
-          var $li = $('li#template').clone().attr('id', null);
-          $li.find('h2').text(documents[doc_name].course.code);
-          $li.find('h3').text(documents[doc_name].course.name);
-          $li.find('.time').text(new Date(documents[doc_name].timestamp).toLocaleString());
-          $li.find('a').attr('href', 'http://noteface.cthomson.ca/dl/latest/' + doc_name + '.pdf');
+          var $tpl = $('li#template').clone().attr('id', null);
+          $tpl.find('h2').text(documents[doc_name].course.code);
+          $tpl.find('h3').text(documents[doc_name].course.name);
+          $tpl.find('.time').text(new Date(documents[doc_name].timestamp).toLocaleString());
+          $tpl.find('a').attr('href', 'http://noteface.cthomson.ca/dl/latest/' + doc_name + '.pdf');
 
-          $('#documents').append($li);
+          if ($("#" + doc_name)) {
+            $("#" + doc_name).append($tpl.children())
+          } else {
+            $('#documents').append($tpl);
+          }
         }
       }
+
+      $("#loading").hide();
     }
   })
 });
