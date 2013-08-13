e146e6fbf6f3ffe6f1933c7efd9835e674295544
Chris Thomson <chris@cthomson.ca>
2013-08-12 22:36:08 -0400

    Style updates for notes.

--- js/notes.js
+++ js/notes.js
@@ -3,13 +3,15 @@ $(document).ready(function() {
     url: 'http://noteface.cthomson.ca/documents.json',
     dataType: 'json',
     success: function(documents) {
-      for (doc in documents) {
-        var $li = $("li#template").clone();
-        $li.attr('id', null);
-        $li.find("h3").text(documents[doc].name);
-        $li.find(".last-modified").text(new Date(documents[doc].timestamp).toLocaleString());
-        $li.find(".sha").text(documents[doc].sha.substr(0, 7));
-        $("#documents").append($li);
+      for (doc_name in documents) {
+        if (documents.hasOwnProperty(doc_name)) {
+          var $li = $('li#template').clone().attr('id', null);
+          $li.find('h2').text("CS 241");
+          $li.find('h3').text('Foundations of Sequential Programs');
+          $li.find('.time').text(new Date(documents[doc_name].timestamp).toLocaleString());
+
+          $('#documents').append($li);
+        }
       }
     }
   })
