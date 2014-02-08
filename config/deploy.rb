d272ddd04212815803c8c772ed282b7aa7f3bc6d
Chris Thomson <chris@cthomson.ca>
2014-02-08 14:25:57 -0500

    Add jekyll to the list of Bundler-installed executables.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :repo_url, 'git@github.com:christhomson/christhomson.github.com.git'
 
 set :deploy_to, "/home/deploy/apps/#{fetch(:application)}"
 
+set :bundle_bins, fetch(:bundle_bins, []).push %w(jekyll)
+
 namespace :deploy do
   task :jekyll_build do
     on roles(:app) do
